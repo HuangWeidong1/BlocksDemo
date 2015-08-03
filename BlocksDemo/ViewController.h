48565dd55a80920490de7ebd77ed44937f845557
nate <nate@nate.local>
2015-08-03 15:34:22 +0800

    Initial Commit

---
+++ BlocksDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocksDemo
+//
+//  Created by nate on 15/8/3.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlocksDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocksDemo
+//
+//  Created by nate on 15/8/3.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
